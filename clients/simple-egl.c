bfdc2f5a34415dc73955832e0a2787d7147b5dcd
Nobuhiko Tanibata <NOBUHIKO_TANIBATA@xddp.denso.co.jp>
2014-03-18 14:19:48 +0900

    clients: modify example clients to support ivi-application.xml
    Signed-off-by: Nobuhiko Tanibata <NOBUHIKO_TANIBATA@xddp.denso.co.jp>

--- clients/simple-egl.c
+++ clients/simple-egl.c
@@ -41,6 +41,10 @@
 #include <EGL/eglext.h>
 
 #include "xdg-shell-client-protocol.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include "protocol/ivi-application-client-protocol.h"
+#define IVI_SURFACE_ID 9000
 
 #ifndef EGL_EXT_swap_buffers_with_damage
 #define EGL_EXT_swap_buffers_with_damage 1
@@ -74,6 +78,7 @@ struct display {
 		EGLConfig conf;
 	} egl;
 	struct window *window;
+	struct ivi_application *ivi_application;
 
 	PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC swap_buffers_with_damage;
 };
@@ -95,6 +100,7 @@ struct window {
 	struct wl_egl_window *native;
 	struct wl_surface *surface;
 	struct xdg_surface *xdg_surface;
+	struct ivi_surface *ivi_surface;
 	EGLSurface egl_surface;
 	struct wl_callback *callback;
 	int fullscreen, opaque, buffer_size, frame_sync;
@@ -255,7 +261,7 @@ init_gl(struct window *window)
 	}
 
 	glUseProgram(program);
-	
+
 	window->gl.pos = 0;
 	window->gl.col = 1;
 
@@ -276,14 +282,16 @@ handle_surface_configure(void *data, struct xdg_surface *surface,
 	uint32_t *p;
 
 	window->fullscreen = 0;
-	wl_array_for_each(p, states) {
+        if (states) {
+            wl_array_for_each(p, states) {
 		uint32_t state = *p;
 		switch (state) {
 		case XDG_SURFACE_STATE_FULLSCREEN:
-			window->fullscreen = 1;
-			break;
+                    window->fullscreen = 1;
+                    break;
 		}
-	}
+            }
+        }
 
 	if (width > 0 && height > 0) {
 		if (!window->fullscreen) {
@@ -301,7 +309,8 @@ handle_surface_configure(void *data, struct xdg_surface *surface,
 				     window->geometry.width,
 				     window->geometry.height, 0, 0);
 
-	xdg_surface_ack_configure(surface, serial);
+        if (window->display && window->display->shell)
+            xdg_surface_ack_configure(surface, serial);
 }
 
 static void
@@ -320,13 +329,8 @@ create_surface(struct window *window)
 {
 	struct display *display = window->display;
 	EGLBoolean ret;
-	
-	window->surface = wl_compositor_create_surface(display->compositor);
-	window->xdg_surface = xdg_shell_get_xdg_surface(display->shell,
-							window->surface);
 
-	xdg_surface_add_listener(window->xdg_surface,
-				 &xdg_surface_listener, window);
+	window->surface = wl_compositor_create_surface(display->compositor);
 
 	window->native =
 		wl_egl_window_create(window->surface,
@@ -337,7 +341,28 @@ create_surface(struct window *window)
 				       display->egl.conf,
 				       window->native, NULL);
 
-	xdg_surface_set_title(window->xdg_surface, "simple-egl");
+	if (display->shell) {
+		window->xdg_surface = xdg_shell_get_xdg_surface(display->shell,
+								window->surface);
+
+		xdg_surface_add_listener(window->xdg_surface,
+					 &xdg_surface_listener, window);
+
+		xdg_surface_set_title(window->xdg_surface, "simple-egl");
+	} else if (display->ivi_application ) {
+		uint32_t id_ivisurf = IVI_SURFACE_ID + (uint32_t)getpid();
+		window->ivi_surface =
+			ivi_application_surface_create(display->ivi_application,
+						       id_ivisurf, window->surface);
+		handle_surface_configure(window, NULL, 250, 250, NULL, 0);
+
+		if (window->ivi_surface == NULL) {
+			fprintf(stderr, "Failed to create ivi_client_surface\n");
+			abort();
+		}
+	} else {
+		assert(0);
+	}
 
 	ret = eglMakeCurrent(window->display->egl.dpy, window->egl_surface,
 			     window->egl_surface, window->display->egl.ctx);
@@ -516,8 +541,13 @@ pointer_handle_button(void *data, struct wl_pointer *wl_pointer,
 	struct display *display = data;
 
 	if (button == BTN_LEFT && state == WL_POINTER_BUTTON_STATE_PRESSED)
-		xdg_surface_move(display->window->xdg_surface,
-				 display->seat, serial);
+	{
+		if (display->shell)
+		{
+			xdg_surface_move(display->window->xdg_surface,
+					 display->seat, serial);
+		}
+	}
 }
 
 static void
@@ -711,6 +741,11 @@ registry_handle_global(void *data, struct wl_registry *registry,
 			// TODO: abort ?
 		}
 	}
+	else if (strcmp(interface, "ivi_application") == 0) {
+		d->ivi_application =
+			wl_registry_bind(registry, name,
+					 &ivi_application_interface, 1);
+	}
 }
 
 static void
@@ -806,6 +841,12 @@ main(int argc, char **argv)
 
 	fprintf(stderr, "simple-egl exiting\n");
 
+	if (window.display->ivi_application)
+	{
+		ivi_surface_destroy(window.ivi_surface);
+		ivi_application_destroy(window.display->ivi_application);
+	}
+
 	destroy_surface(&window);
 	fini_egl(&display);
 
@@ -820,6 +861,9 @@ main(int argc, char **argv)
 		wl_compositor_destroy(display.compositor);
 
 	wl_registry_destroy(display.registry);
+	if (window.display->ivi_application)
+		wl_display_roundtrip(display.display);
+
 	wl_display_flush(display.display);
 	wl_display_disconnect(display.display);
 
--- clients/window.c
+++ clients/window.c
@@ -72,6 +72,10 @@ typedef void *EGLContext;
 
 #include "window.h"
 
+#include <sys/types.h>
+#include "ivi-application-client-protocol.h"
+#define IVI_SURFACE_ID 9000
+
 struct shm_pool;
 
 struct global {
@@ -133,6 +137,7 @@ struct display {
 
 	int has_rgb565;
 	int seat_version;
+	struct ivi_application *ivi_application;
 };
 
 struct window_output {
@@ -248,6 +253,7 @@ struct window {
 
 	struct window *parent;
 	struct wl_surface *last_parent_surface;
+	struct ivi_surface *ivi_surface;
 
 	struct window_frame *frame;
 
@@ -1384,6 +1390,20 @@ surface_create_surface(struct surface *surface, uint32_t flags)
 	struct display *display = surface->window->display;
 	struct rectangle allocation = surface->allocation;
 
+	if (display->ivi_application)
+	{
+		if (!surface->toysurface) {
+			uint32_t id_ivisurf = IVI_SURFACE_ID + (uint32_t)getpid();
+			surface->window->ivi_surface =
+				ivi_application_surface_create(display->ivi_application,
+							       id_ivisurf, surface->surface);
+			if (surface->window->ivi_surface == NULL) {
+				fprintf(stderr, "Failed to create ivi_client_surface\n");
+				abort();
+			}
+		}
+	}
+
 	if (!surface->toysurface && display->dpy &&
 	    surface->buffer_type == WINDOW_BUFFER_TYPE_EGL_WINDOW) {
 		surface->toysurface =
@@ -1487,6 +1507,12 @@ surface_destroy(struct surface *surface)
 	if (surface->toysurface)
 		surface->toysurface->destroy(surface->toysurface);
 
+	if (surface->window->display->ivi_application)
+	{
+		ivi_surface_destroy(surface->window->ivi_surface);
+		ivi_application_destroy(surface->window->display->ivi_application);
+	}
+
 	wl_list_remove(&surface->link);
 	free(surface);
 }
@@ -1501,7 +1527,7 @@ window_destroy(struct window *window)
 
 	wl_list_remove(&window->redraw_task.link);
 
-	wl_list_for_each(input, &display->input_list, link) {	  
+	wl_list_for_each(input, &display->input_list, link) {
 		if (input->touch_focus == window)
 			input->touch_focus = NULL;
 		if (input->pointer_focus == window)
@@ -3051,7 +3077,7 @@ touch_handle_down(void *data, struct wl_touch *wl_touch,
 			wl_list_insert(&input->touch_point_list, &tp->link);
 
 			if (widget->touch_down_handler)
-				(*widget->touch_down_handler)(widget, input, 
+				(*widget->touch_down_handler)(widget, input,
 							      serial, time, id,
 							      sx, sy,
 							      widget->user_data);
@@ -4489,7 +4515,7 @@ window_create_internal(struct display *display, int custom)
 	surface = surface_create(window);
 	window->main_surface = surface;
 
-	assert(custom || display->xdg_shell);
+	assert(custom || display->xdg_shell || display->ivi_application);
 
 	window->custom = custom;
 	window->preferred_format = WINDOW_PREFERRED_FORMAT_NONE;
@@ -4512,14 +4538,17 @@ window_create(struct display *display)
 
 	window = window_create_internal(display, 0);
 
-	window->xdg_surface =
-		xdg_shell_get_xdg_surface(window->display->xdg_shell,
-					  window->main_surface->surface);
-	fail_on_null(window->xdg_surface);
+	if (window->display->xdg_shell)
+	{
+		window->xdg_surface =
+			xdg_shell_get_xdg_surface(window->display->xdg_shell,
+						  window->main_surface->surface);
+		fail_on_null(window->xdg_surface);
 
-	xdg_surface_set_user_data(window->xdg_surface, window);
-	xdg_surface_add_listener(window->xdg_surface,
-				 &xdg_surface_listener, window);
+		xdg_surface_set_user_data(window->xdg_surface, window);
+		xdg_surface_add_listener(window->xdg_surface,
+					 &xdg_surface_listener, window);
+	}
 
 	return window;
 }
@@ -4772,19 +4801,22 @@ window_show_menu(struct display *display,
 
 	frame_interior(menu->frame, &ix, &iy, NULL, NULL);
 
-	window->xdg_popup = xdg_shell_get_xdg_popup(display->xdg_shell,
-						    window->main_surface->surface,
-						    parent->main_surface->surface,
-						    input->seat,
-						    display_get_serial(window->display),
-						    window->x - ix,
-						    window->y - iy,
-						    0);
-	fail_on_null(window->xdg_popup);
+	if (display->xdg_shell)
+	{
+		window->xdg_popup = xdg_shell_get_xdg_popup(display->xdg_shell,
+							    window->main_surface->surface,
+							    parent->main_surface->surface,
+							    input->seat,
+							    display_get_serial(window->display),
+							    window->x - ix,
+							    window->y - iy,
+							    0);
+		fail_on_null(window->xdg_popup);
 
-	xdg_popup_set_user_data(window->xdg_popup, window);
-	xdg_popup_add_listener(window->xdg_popup,
-			       &xdg_popup_listener, window);
+		xdg_popup_set_user_data(window->xdg_popup, window);
+		xdg_popup_add_listener(window->xdg_popup,
+				       &xdg_popup_listener, window);
+	}
 }
 
 void
@@ -5231,6 +5263,11 @@ registry_handle_global(void *data, struct wl_registry *registry, uint32_t id,
 			wl_registry_bind(registry, id,
 					 &wl_subcompositor_interface, 1);
 	}
+	else if (strcmp(interface, "ivi_application") == 0) {
+		d->ivi_application =
+			wl_registry_bind(registry, id,
+					 &ivi_application_interface, 1);
+	}
 
 	if (d->global_handler)
 		d->global_handler(d, id, interface, version, d->user_data);
@@ -5539,6 +5576,9 @@ display_destroy(struct display *display)
 	wl_registry_destroy(display->registry);
 
 	close(display->epoll_fd);
+
+	if (display->ivi_application)
+		wl_display_roundtrip(display->display);
 
 	if (!(display->display_fd_events & EPOLLERR) &&
 	    !(display->display_fd_events & EPOLLHUP))
