8855726cb4bae9538b6dbc5cc6bb8d9499b76ee3
Manuel Bachmann <manuel.bachmann@open.eurogiciel.org>
2014-06-30 13:35:29 +0200

    Provide IVI-Shell extensions functions again
    "ivi-shell-ext.h" functions do not exist upstream, but seem to be
    needed by ICO shell. Adapt them to new 1.5 API.
    
    Bug-Tizen: TC-211
    
    Change-Id: Ia9bdb16053ee1309c62d16279fdd2a919e0378a5
    Signed-off-by: Manuel Bachmann <manuel.bachmann@open.eurogiciel.org>

---
+++ ivi-shell/ivi-shell-ext.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2014 DENSO CORPORATION
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and
+ * its documentation for any purpose is hereby granted without fee, provided
+ * that the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of the copyright holders not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.  The copyright holders make
+ * no representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS, IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef _IVI_SHELL_EXT_H_
+#define _IVI_SHELL_EXT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include "compositor.h"
+#include <wayland-util.h>
+struct shell_surface;
+
+int
+init_ivi_shell_ext(struct weston_compositor *ec, int *argc, char *argv[]);
+
+void
+ivi_shell_get_shell_surfaces(struct wl_array *surfaces);
+
+uint32_t
+shell_surface_get_process_id(struct shell_surface *surface);
+
+char*
+shell_surface_get_title(struct shell_surface* surface);
+
+struct weston_surface*
+shell_surface_get_surface(struct shell_surface* surface);
+
+#endif
