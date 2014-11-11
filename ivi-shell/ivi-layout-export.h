3f9bacf5354ca4acd998e61ae9be86b845b0a57f
Nobuhiko Tanibata <NOBUHIKO_TANIBATA@xddp.denso.co.jp>
2014-11-11 15:32:33 +0900

    ivi-layout: ivi-layout-export.h remove type of ivi_layout_layer_properties
    Signed-off-by: Nobuhiko Tanibata <NOBUHIKO_TANIBATA@xddp.denso.co.jp>

--- ivi-shell/ivi-layout-export.h
+++ ivi-shell/ivi-layout-export.h
@@ -96,7 +96,6 @@ struct ivi_layout_layer_properties
     int32_t  dest_height;
     enum wl_output_transform orientation;
     uint32_t visibility;
-    uint32_t type;
     int32_t  transition_type;
     uint32_t transition_duration;
     double   start_alpha;
