07c0c96b193d3058bc7909e60bec06737bd879ce
Nobuhiko Tanibata <NOBUHIKO_TANIBATA@xddp.denso.co.jp>
2015-04-24 16:44:21 +0900

    tests: [PATCH 07/14 part1] rename to test_layer_bad_remove
    > "bad_create"? Isn't this a bad remove?
    
    Signed-off-by: Nobuhiko Tanibata <NOBUHIKO_TANIBATA@xddp.denso.co.jp>

--- tests/ivi_layout-internal-test.c
+++ tests/ivi_layout-internal-test.c
@@ -410,7 +410,7 @@ test_layer_source_rectangle(struct test_context *ctx)
 }
 
 static void
-test_layer_bad_create(struct test_context *ctx)
+test_layer_bad_remove(struct test_context *ctx)
 {
 	const struct ivi_controller_interface *ctl = ctx->controller_interface;
 	ctl->layer_remove(NULL);
@@ -720,7 +720,7 @@ run_internal_tests(void *data)
 	test_layer_position(ctx);
 	test_layer_destination_rectangle(ctx);
 	test_layer_source_rectangle(ctx);
-	test_layer_bad_create(ctx);
+	test_layer_bad_remove(ctx);
 	test_layer_bad_visibility(ctx);
 	test_layer_bad_opacity(ctx);
 	test_layer_bad_destination_rectangle(ctx);
