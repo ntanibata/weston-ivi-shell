b04972ce22b1653c8655062f26cbc02d88fe5ec4
Nobuhiko Tanibata <NOBUHIKO_TANIBATA@xddp.denso.co.jp>
2015-04-24 10:20:04 +0900

    tests: [PATCH 11/14 part6] add/remove empty line
    > Empty line.
    > Needs empty line here.
    
    Signed-off-by: Nobuhiko Tanibata <NOBUHIKO_TANIBATA@xddp.denso.co.jp>

--- tests/ivi_layout-test-plugin.c
+++ tests/ivi_layout-test-plugin.c
@@ -901,6 +901,7 @@ test_surface_add_notification_callback(struct ivi_layout_surface *ivisurf,
 {
 	struct test_context *ctx = userdata;
 	const struct ivi_controller_interface *ctl = ctx->controller_interface;
+
 	runner_assert_or_return(ctl->get_id_of_surface(ivisurf) == IVI_TEST_SURFACE_ID(0));
 
 	ctx->user_flags = 1;
@@ -934,6 +935,7 @@ test_surface_configure_notification_callback(struct ivi_layout_surface *ivisurf,
 {
 	struct test_context *ctx = userdata;
 	const struct ivi_controller_interface *ctl = ctx->controller_interface;
+
 	runner_assert_or_return(ctl->get_id_of_surface(ivisurf) == IVI_TEST_SURFACE_ID(0));
 
 	ctx->user_flags = 1;
@@ -942,6 +944,7 @@ test_surface_configure_notification_callback(struct ivi_layout_surface *ivisurf,
 RUNNER_TEST(surface_configure_notification_p1)
 {
 	const struct ivi_controller_interface *ctl = ctx->controller_interface;
+
 	runner_assert(IVI_SUCCEEDED == ctl->add_notification_configure_surface(test_surface_configure_notification_callback, ctx));
 	ctl->commit_changes();
 
@@ -1005,6 +1008,7 @@ test_surface_remove_notification_callback(struct ivi_layout_surface *ivisurf,
 RUNNER_TEST(surface_remove_notification_p1)
 {
 	const struct ivi_controller_interface *ctl = ctx->controller_interface;
+
 	runner_assert(ctl->add_notification_remove_surface(
 		      test_surface_remove_notification_callback, ctx) == IVI_SUCCEEDED);
 
